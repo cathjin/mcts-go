2ceb3548fe6cf6b0172c24ee809c56737944d439
Catherine <catherine.jin06@gmail.com>
2025-10-04 01:52:36 -0400

    rough draft of tromp taylor scoring

--- go.py
+++ go.py
@@ -1,86 +1,142 @@
-NUM_COLS = 9
-NUM_ROWS = 9
+class Go:
+    def __init__(self, num_rows = 19, num_cols = 19):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.board = [["O"]*num_cols for _ in range(num_rows)]
+        self.prev_state = [["O"]*num_cols for _ in range(num_rows)]
 
-BOARD = [["O"]*NUM_COLS for _ in range(NUM_ROWS)]
+    def print_board(self):
+        for i in range(self.num_rows):
+            for j in range(self.num_cols):
+                print(self.board[i][j], end="")
+                if (j != self.num_cols - 1):
+                    print(" - ", end = "")
+                else:
+                    print()
+                    if (i != self.num_rows - 1):
+                        print("|   " * self.num_cols)
 
-def print_board():
-    for i in range(NUM_ROWS):
-        for j in range(NUM_COLS):
-            print(BOARD[i][j], end="")
-            if (j != NUM_COLS - 1):
-                print(" - ", end = "")
-            else:
-                print()
-                if (i != NUM_ROWS - 1):
-                    print("|   " * NUM_COLS)
+    def play_game(self):
+        while(True):
+            # self.prev_board = self.board # need to make deep copy
 
-def play_game():
-    while(True):
-        move_x = input("Black's Move Row: ")
-        move_y = input("Black's Move Column: ")
-        BOARD[int(move_x) - 1][int(move_y) - 1] = "B"
-        check_all_captured("W", "B")
-        print_board()
-
-
-        move_x = input("White's Move Row: ")
-        move_y = input("White's Move Column: ")
-        BOARD[int(move_x) - 1][int(move_y) - 1] = "W"
-        check_all_captured("B", "W")
-        print_board()
-
-def check_all_captured(player, opp):
-    indices = []
-    for i, row in enumerate(BOARD):
-        for j, element in enumerate(row):
-            if element == player:
-                indices.append((i, j))  # row, col
-    captured = []
-    for x,y in indices:
-        if check_captured(player, opp, x,y):
-            captured.append((x,y))
-    print("Captured", captured)
-    for x,y in captured:
-        BOARD[x][y] = "O"
-    return captured
-
-def check_captured(player, opp, x, y, checked = []):
-    neighbour_indices = get_neighbours_indices(x,y,player)
-    neighbour_symbols = get_neighbours_symbol(neighbour_indices)
-    if "O" in neighbour_symbols:
-        return False
-    else:
-        for i in range(len(neighbour_symbols)):
-            if neighbour_symbols[i] == player and neighbour_indices[i] not in checked:
-                x1, y1 = neighbour_indices[i]
-                checked.append((x,y))
-                return check_captured(player, opp, x1, y1)
-        return True
+            move_x = input("Black's Move Row: ")
+            move_y = input("Black's Move Column: ")
+            self.board[int(move_x) - 1][int(move_y) - 1] = "B"
+            self.check_all_captured("W", "B")
+            self.print_board()
 
 
+            move_x = input("White's Move Row: ")
+            move_y = input("White's Move Column: ")
+            self.board[int(move_x) - 1][int(move_y) - 1] = "W"
+            self.check_all_captured("B", "W")
+            self.print_board()
 
-def get_neighbours_indices(x, y, player):
-    if x== 0 and y== 0:
-        return [(1, 0), (0, 1)] 
-    elif x == NUM_ROWS - 1 and y== NUM_COLS - 1:
-        return [(NUM_ROWS-2, NUM_COLS-1), (NUM_ROWS-1, NUM_COLS - 2)]
-    elif x == 0 and y == NUM_COLS-1:
-        return [(1, NUM_COLS-1), (0, NUM_COLS-2)]
-    elif x == NUM_ROWS-1 and y== 0:
-        return[(NUM_ROWS - 2, 0), (NUM_ROWS-1, 1)]
-    elif x == NUM_ROWS -1:
-        return[(NUM_ROWS - 2, y), (NUM_ROWS-1, y-1), (NUM_ROWS-1, y + 1)]
-    elif y == NUM_COLS -1:
-        return[(x - 1, NUM_COLS-1), (x, NUM_COLS-2), (x + 1, NUM_COLS-1)]
-    else:
-        return[(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
-
-def get_neighbours_symbol(neighbour_indices):
-    symbols = []
-    for x, y in neighbour_indices:
-        print(x,y)
-        symbols.append(BOARD[x][y])
-    return symbols
+            self.score()
+            # if self.prev_board == self.board:
+                # break # change later
 
 
-play_game()
+    def check_all_captured(self, player, opp):
+        indices = []
+        for i, row in enumerate(self.board):
+            for j, element in enumerate(row):
+                if element == player:
+                    indices.append((i, j))  # row, col
+        captured = []
+        for x,y in indices:
+            if self.check_captured(player, opp, x,y):
+                captured.append((x,y))
+        print("Captured", captured)
+        for x,y in captured:
+            self.board[x][y] = "O"
+        return captured
+
+    # needs reworking
+    def check_captured(self, player, opp, x, y, checked = []):
+        neighbour_indices = self.get_neighbours_indices(x,y)
+        neighbour_symbols = self.get_neighbours_symbol(neighbour_indices)
+        if "O" in neighbour_symbols:
+            return False
+        else:
+            for i in range(len(neighbour_symbols)):
+                if neighbour_symbols[i] == player and neighbour_indices[i] not in checked:
+                    x1, y1 = neighbour_indices[i]
+                    checked.append((x,y))
+                    return self.check_captured(player, opp, x1, y1)
+            return True
+
+
+
+    def get_neighbours_indices(self, x, y):
+        if x== 0 and y== 0: # (0,0)
+            return [(1, 0), (0, 1)] 
+        elif x == self.num_rows - 1 and y== self.num_cols - 1:
+            return [(self.num_rows-2, self.num_cols-1), (self.num_rows-1, self.num_cols - 2)]
+        elif x == 0 and y == self.num_cols-1: 
+            return [(1, self.num_cols-1), (0, self.num_cols-2)]
+        elif x == self.num_rows-1 and y== 0:
+            return[(self.num_rows - 2, 0), (self.num_rows-1, 1)]
+        elif x == self.num_rows -1:
+            return[(self.num_rows - 2, y), (self.num_rows-1, y-1), (self.num_rows-1, y + 1)]
+        elif y == self.num_cols -1:
+            return[(x - 1, self.num_cols-1), (x, self.num_cols-2), (x + 1, self.num_cols-1)]
+        elif x == 0:
+            return[(0, y - 1), (0, y + 1), (1, y)]
+        elif y == 0:
+            return[(x - 1, 0), (x + 1, 0), (x, 1)]
+        else:
+            return[(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+
+    def get_neighbours_symbol(self, neighbour_indices):
+        symbols = []
+        for x, y in neighbour_indices:
+            symbols.append(self.board[x][y])
+        return symbols
+
+    def score(self):
+        black_score = 0
+        white_score = 0
+
+        # count num stones
+        checked = set()
+
+        def score_area(x, y):
+            to_visit = [(x,y)]
+            points = 0
+            symbols = set()
+            
+            while to_visit:
+                check_x, check_y = to_visit.pop()
+                if (check_x, check_y) in checked:
+                    continue
+                checked.add((check_x, check_y))
+                points += 1
+                neighbour_indices = self.get_neighbours_indices(check_x, check_y)
+                neighbour_symbols = self.get_neighbours_symbol(neighbour_indices)
+                for i in range(len(neighbour_symbols)):
+                    if neighbour_symbols[i] == "O":
+                        to_visit.append(neighbour_indices[i])
+                    else:
+                        symbols.add(neighbour_symbols[i])
+            return points, symbols
+        for i in range(self.num_rows - 1):
+            for j in range(self.num_cols - 1):
+                if self.board[i][j] == "B":
+                    black_score += 1
+                elif self.board[i][j] == "W":
+                    white_score += 1
+                else:
+                    count, symbols = score_area(i, j)
+                    if len(symbols) == 2:
+                        print (symbols)
+                        continue
+                    elif "B" in symbols:
+                        black_score += count
+                    elif "W" in symbols:
+                        white_score += count
+        print("BLACK SCORE: ", black_score)
+        print("WHITE SCORE: ", white_score)
+game = Go(9,9)
+game.play_game()
